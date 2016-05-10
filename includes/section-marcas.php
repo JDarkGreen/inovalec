74775e4507ec3963b7c34704304032298d79be31
JDarkGreen <jgomez.4net@gmail.com>
2016-05-10 16:56:54 -0500

    update home

--- includes/section-marcas.php
+++ includes/section-marcas.php
@@ -2,35 +2,37 @@
 
 <section class="sectionMarcas">
   <div id="owl-carousel-marcas" class="">
-    <div class="item"><img src="images/carousel-marcas/ABB.PNG" alt="" class="responsive-img" /></div>
-    <div class="item"><img src="images/carousel-marcas/BTICINO.png" alt="" class="responsive-img" /></div>
 
-    <div class="item"><img src="images/carousel-marcas/SCHNEIDER.png" alt="" class="responsive-img" /></div>      
+    <div class="item"><img src="images/carousel-marcas/ABB.PNG" alt="" class="" /></div>
 
-    <div class="item"><img src="images/carousel-marcas/SIEMENS.png" alt="" class="responsive-img" /></div>
+    <div class="item"><img src="images/carousel-marcas/BTICINO.png" alt="" class="" /></div>
 
-    <div class="item"><img src="images/carousel-marcas/INDECO.jpg" alt="" class="responsive-img" /></div>
+    <div class="item"><img src="images/carousel-marcas/SCHNEIDER.png" alt="" class="" /></div>      
 
-    <div class="item"><img src="images/carousel-marcas/marca-3m-jbg.jpg" alt="" class="responsive-img" /></div>
+    <div class="item"><img src="images/carousel-marcas/SIEMENS.png" alt="" class="" /></div>
 
-    <div class="item"><img src="images/carousel-marcas/LS.png" alt="" class="responsive-img" /></div>
+    <div class="item"><img src="images/carousel-marcas/INDECO.jpg" alt="" class="" /></div>
 
-    <div class="item"><img src="images/carousel-marcas/CRC.jpg" alt="" class="responsive-img" /></div>
+    <div class="item"><img src="images/carousel-marcas/marca-3m-jbg.jpg" alt="" class="" /></div>
 
-    <div class="item"><img src="images/carousel-marcas/GENERAL ELECTRIC.jpg" alt="" class="responsive-img" /></div>
+    <div class="item"><img src="images/carousel-marcas/LS.png" alt="" class="" /></div>
 
-    <div class="item"><img src="images/carousel-marcas/KSS.png" alt="" class="responsive-img" /></div>
-    <div class="item"><img src="images/carousel-marcas/LEGRAND.png" alt="" class="responsive-img" /></div>
+    <div class="item"><img src="images/carousel-marcas/CRC.jpg" alt="" class="" /></div>
 
-    <div class="item"><img src="images/carousel-marcas/marca-ide-jbg.jpg" alt="" class="responsive-img" /></div>
+    <div class="item"><img src="images/carousel-marcas/GENERAL ELECTRIC.jpg" alt="" class="" /></div>
 
-    <div class="item"><img src="images/carousel-marcas/marca-amp-jbg.jpg" alt="" class="responsive-img" /></div>
+    <div class="item"><img src="images/carousel-marcas/KSS.png" alt="" class="" /></div>
+    <div class="item"><img src="images/carousel-marcas/LEGRAND.png" alt="" class="" /></div>
 
-    <div class="item"><img src="images/carousel-marcas/marca-leviton-jbg.jpg" alt="" class="responsive-img" /></div>
+    <div class="item"><img src="images/carousel-marcas/marca-ide-jbg.jpg" alt="" class="" /></div>
+
+    <div class="item"><img src="images/carousel-marcas/marca-amp-jbg.jpg" alt="" class="" /></div>
+
+    <div class="item"><img src="images/carousel-marcas/marca-leviton-jbg.jpg" alt="" class="" /></div>
     
-    <div class="item"><img src="images/carousel-marcas/PHELPS_DODGE.png" alt="" class="responsive-img" /></div> 
+    <div class="item"><img src="images/carousel-marcas/PHELPS_DODGE.png" alt="" class="" /></div> 
 
-    <div class="item"><img src="images/carousel-marcas/PHILIPS.gif" alt="" class="responsive-img" /></div>  
+    <div class="item"><img src="images/carousel-marcas/PHILIPS.gif" alt="" class="" /></div>  
       
   </div><!-- /.owl-carousel -->
 </section><!-- /sectionMarcas -->
